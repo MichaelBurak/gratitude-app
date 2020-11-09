15ecc08692f5f40aeb2a63062e0f1f8ab41c1a12
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-11-09 02:59:07 -0700

    retain focus false fixes dialog crashes

--- src/components/GratefulList.vue
+++ src/components/GratefulList.vue
@@ -13,6 +13,7 @@
       dark
    />
    <v-card-title>I am Grateful for:</v-card-title>
+    
     <v-list >
         <div @drop='onDrop($event, 1)' 
       @dragover.prevent
@@ -35,6 +36,48 @@
   
 
               <!-- </v-align> -->
+
+              <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+      :retain-focus="false"
+    >
+      <!-- <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template> -->
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          You are Grateful for:
+        </v-card-title>
+
+        <v-card-text>
+          {{newestTitle}}!
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = !dialog"
+          >
+            Exit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
     
             <v-divider></v-divider>
             
@@ -101,6 +144,8 @@
 import ItemForm from "./ItemForm"
   export default {
     data: () => ({
+        dialog:false,
+        newestTitle:"",
       items: [
         //   {title:"Friends", list:1},
         //   {title:"Drop Items Here:", list:2}
@@ -118,6 +163,8 @@ import ItemForm from "./ItemForm"
    methods:{
        newItem(item){
            this.items.push({title:item, list:1})
+           this.dialog = !this.dialog
+           this.newestTitle = item
        },
        newIdea(item){
            this.items.push({title:item, list:2})
